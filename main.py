6a4abd28b79e175b5b13890a1d785551c868ff56
= <=>
2022-12-12 12:09:10 -0300

    energies

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ h = int(input())
 
 Ep = numpy.round(fisica.PotentialEnergyG(m, h),2)
 Em = numpy.round(fisica.MechanicalEnergy(Ep),2)
+
 print("Energia Potencial: " + str(Ep) + "N")
 print("Energia Cinética: " + str(Ep) + "N")
 print("Energia Mecânica: " + str(Em) + "N")
